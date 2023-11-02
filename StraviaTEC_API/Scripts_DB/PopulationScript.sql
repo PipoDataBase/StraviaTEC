d2e61370cfcd4127aa77efd2a83e61341609c9fa
CAMANEM <camanem10@hotmail.com>
2023-11-02 16:50:06 -0600

    DB | Population Script with nationalities
    + Added Nationalities

---
+++ StraviaTEC_API/Scripts_DB/PopulationScript.sql
@@ -0,0 +1,343 @@
+-- ================================================
+--                  Nationalities
+-- ================================================
+
+EXEC spInsertNationality Afghan;
+Go
+
+EXEC spInsertNationality Albanian;
+Go
+
+EXEC spInsertNationality Algerian;
+Go
+
+EXEC spInsertNationality Argentine;
+Go
+
+EXEC spInsertNationality Argentinian;
+Go
+
+EXEC spInsertNationality Australian;
+Go
+
+EXEC spInsertNationality Austrian;
+Go
+
+EXEC spInsertNationality Bangladeshi;
+Go
+
+EXEC spInsertNationality Belgian;
+Go
+
+EXEC spInsertNationality Bolivian;
+Go
+
+EXEC spInsertNationality Batswana;
+Go
+
+EXEC spInsertNationality Brazilian;
+Go
+
+EXEC spInsertNationality Bulgarian;
+Go
+
+EXEC spInsertNationality Cambodian;
+Go
+
+EXEC spInsertNationality Cameroonian;
+Go
+
+EXEC spInsertNationality Canadian;
+Go
+
+EXEC spInsertNationality Chilean;
+Go
+
+EXEC spInsertNationality Chinese;
+Go
+
+EXEC spInsertNationality Colombian;
+Go
+
+EXEC spInsertNationality Costa Rican;
+Go
+
+EXEC spInsertNationality Croatian;
+Go
+
+EXEC spInsertNationality Cuban;
+Go
+
+EXEC spInsertNationality Czech;
+Go
+
+EXEC spInsertNationality Danish;
+Go
+
+EXEC spInsertNationality Dominican;
+Go
+
+EXEC spInsertNationality Ecuadorian;
+Go
+
+EXEC spInsertNationality Egyptian;
+Go
+
+EXEC spInsertNationality Salvadorian;
+Go
+
+EXEC spInsertNationality English;
+Go
+
+EXEC spInsertNationality Estonian;
+Go
+
+EXEC spInsertNationality Ethiopian;
+Go
+
+EXEC spInsertNationality Fijian;
+Go
+
+EXEC spInsertNationality Finnish;
+Go
+
+EXEC spInsertNationality French;
+Go
+
+EXEC spInsertNationality German;
+Go
+
+EXEC spInsertNationality Ghanaian;
+Go
+
+EXEC spInsertNationality Greek;
+Go
+
+EXEC spInsertNationality Guatemalan;
+Go
+
+EXEC spInsertNationality Haitian;
+Go
+
+EXEC spInsertNationality Honduran;
+Go
+
+EXEC spInsertNationality Hungarian;
+Go
+
+EXEC spInsertNationality Icelandic;
+Go
+
+EXEC spInsertNationality Indian;
+Go
+
+EXEC spInsertNationality Indonesian;
+Go
+
+EXEC spInsertNationality Iranian;
+Go
+
+EXEC spInsertNationality Iraqi;
+Go
+
+EXEC spInsertNationality Irish;
+Go
+
+EXEC spInsertNationality Israeli;
+Go
+
+EXEC spInsertNationality Italian;
+Go
+
+EXEC spInsertNationality Jamaican;
+Go
+
+EXEC spInsertNationality Japanese;
+Go
+
+EXEC spInsertNationality Jordanian;
+Go
+
+EXEC spInsertNationality Kenyan;
+Go
+
+EXEC spInsertNationality Kuwaiti;
+Go
+
+EXEC spInsertNationality Lao;
+Go
+
+EXEC spInsertNationality Latvian;
+Go
+
+EXEC spInsertNationality Lebanese;
+Go
+
+EXEC spInsertNationality Libyan;
+Go
+
+EXEC spInsertNationality Lithuanian;
+Go
+
+EXEC spInsertNationality Malagasy;
+Go
+
+EXEC spInsertNationality Malaysian;
+Go
+
+EXEC spInsertNationality Malian;
+Go
+
+EXEC spInsertNationality Maltese;
+Go
+
+EXEC spInsertNationality Mexican;
+Go
+
+EXEC spInsertNationality Mongolian;
+Go
+
+EXEC spInsertNationality Moroccan;
+Go
+
+EXEC spInsertNationality Mozambican;
+Go
+
+EXEC spInsertNationality Namibian;
+Go
+
+EXEC spInsertNationality Nepalese;
+Go
+
+EXEC spInsertNationality Dutch;
+Go
+
+EXEC spInsertNationality New Zealand;
+Go
+
+EXEC spInsertNationality Nicaraguan;
+Go
+
+EXEC spInsertNationality Nigerian;
+Go
+
+EXEC spInsertNationality Norwegian;
+Go
+
+EXEC spInsertNationality Pakistani;
+Go
+
+EXEC spInsertNationality Panamanian;
+Go
+
+EXEC spInsertNationality Paraguayan;
+Go
+
+EXEC spInsertNationality Peruvian;
+Go
+
+EXEC spInsertNationality Philippine;
+Go
+
+EXEC spInsertNationality Polish;
+Go
+
+EXEC spInsertNationality Portuguese;
+Go
+
+EXEC spInsertNationality Romanian;
+Go
+
+EXEC spInsertNationality Russian;
+Go
+
+EXEC spInsertNationality Saudi;
+Go
+
+EXEC spInsertNationality Scottish;
+Go
+
+EXEC spInsertNationality Senegalese;
+Go
+
+EXEC spInsertNationality Serbian;
+Go
+
+EXEC spInsertNationality Singaporean;
+Go
+
+EXEC spInsertNationality Slovak;
+Go
+
+EXEC spInsertNationality South African;
+Go
+
+EXEC spInsertNationality Korean;
+Go
+
+EXEC spInsertNationality Spanish;
+Go
+
+EXEC spInsertNationality Sri Lankan;
+Go
+
+EXEC spInsertNationality Sudanese;
+Go
+
+EXEC spInsertNationality Swedish;
+Go
+
+EXEC spInsertNationality Swiss;
+Go
+
+EXEC spInsertNationality Syrian;
+Go
+
+EXEC spInsertNationality Taiwanese;
+Go
+
+EXEC spInsertNationality Tajikistani;
+Go
+
+EXEC spInsertNationality Thai;
+Go
+
+EXEC spInsertNationality Tongan;
+Go
+
+EXEC spInsertNationality Tunisian;
+Go
+
+EXEC spInsertNationality Turkish;
+Go
+
+EXEC spInsertNationality Ukrainian;
+Go
+
+EXEC spInsertNationality Emirati;
+Go
+
+EXEC spInsertNationality British;
+Go
+
+EXEC spInsertNationality American;
+Go
+
+EXEC spInsertNationality Uruguayan;
+Go
+
+EXEC spInsertNationality Venezuelan;
+Go
+
+EXEC spInsertNationality Vietnamese;
+Go
+
+EXEC spInsertNationality Welsh;
+Go
+
+EXEC spInsertNationality Zambian;
+Go
+
+EXEC spInsertNationality Zimbabwean;
+Go
+
