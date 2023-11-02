94c7ba45d1b679a2d2ea1ee5a1b689e9bbdb5774
Jose Andrés Rodríguez Rojas <joseandres216@gmail.com>
2023-11-02 17:42:13 -0600

    Search view skelleton
    Done an initial skelleton for the search view

--- StraviaTEC_WebApp/src/app/demo/components/search/search.component.ts
+++ StraviaTEC_WebApp/src/app/demo/components/search/search.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 
+export interface Sportman {
+  username: string;
+  name: string;
+  lastName1: string;
+  lastName2: string;
+  birthDate: string;
+  photoPath: string;
+  password: string;
+  nationalityId: number;
+}
+
+export interface Group {
+  name: string
+}
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,4 +22,125 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent {
 
+  // Functional variables
+
+  private loadingAnimation = false;
+  public getLoadingAnimation() {
+    return this.loadingAnimation;
+  }
+  public setLoadingAnimation(value) {
+    this.loadingAnimation = value;
+  }
+
+  private showSearchResults = false;
+  public getShowSearchResults() {
+    return this.showSearchResults;
+  }
+  public setShowSearchResults(value) {
+    this.showSearchResults = value;
+  }
+
+  // Database variables
+
+  private foundUsers: Sportman[];
+  public getFoundUsers(): Sportman[] {
+    return this.foundUsers;
+  }
+  public setFoundUsers(value: Sportman[]) {
+    this.foundUsers = value;
+  }
+
+  private foundGroups: Group[];
+  public getFoundGroups(): Group[] {
+    return this.foundGroups;
+  }
+  public setFoundGroups(value: Group[]) {
+    this.foundGroups = value;
+  }
+
+  //Other variables
+
+
+  // Constructor
+
+
+  // Search button function: Searches on database for a match of any user or group
+  searchButtonOnClick(searchInputValue: String) {
+    this.loadingAnimation = true;
+    setTimeout(() => {
+    this.loadingAnimation = false
+    
+    console.log("Searching '" + searchInputValue + "'...");
+
+    // Database get
+
+    // Search script for testing
+    this.foundUsers = [
+      {username: 'joseandres',
+      name:'Andres',
+      lastName1:'Rodriguez',
+      lastName2:'R',
+      birthDate:'',
+      photoPath:'../../../../assets/straviatec/default-avatar.png',
+      password:"",
+      nationalityId:0},
+      {username: 'emarin',
+      name:'Ema',
+      lastName1:'Marin',
+      lastName2:'G',
+      birthDate:'',
+      photoPath:'../../../../assets/straviatec/default-avatar.png',
+      password:"",
+      nationalityId:0},
+      {username: 'seballoll',
+      name:'Sebas',
+      lastName1:'Chen',
+      lastName2:'C',
+      birthDate:'',
+      photoPath:'../../../../assets/straviatec/default-avatar.png',
+      password:"",
+      nationalityId:0},
+      {username: 'camanem',
+      name:'Oscar',
+      lastName1:'Soto',
+      lastName2:'?',
+      birthDate:'',
+      photoPath:'../../../../assets/straviatec/default-avatar.png',
+      password:"",
+      nationalityId:0},
+    ]
+
+    this.foundGroups = [
+      {name: 'Group1'},
+      {name: 'Group2'},
+      {name: 'Group3'},
+      {name: 'Group4'},
+      {name: 'Group5'},
+      {name: 'Group6'},
+      {name: 'Group7'},
+      {name: 'Group8'},
+      {name: 'Group9'},
+      {name: 'Group10'},
+      {name: 'Group11'},
+      {name: 'Group12'},
+    ]
+
+    this.showSearchResults = true
+    
+    }, 
+    1000);
+  }
+
+  // Follow button function
+  followUserButtonOnClick(username: string){
+    // Database request
+
+    console.log("Following user: " + username)
+  }
+
+  submitGroupButtonOnClick(name: string){
+    // Database request
+
+    console.log("Submited group: " + name)
+  }
 }
--- StraviaTEC_WebApp/src/app/demo/components/search/search.module.ts
+++ StraviaTEC_WebApp/src/app/demo/components/search/search.module.ts
@@ -2,11 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchComponent } from './search.component';
+import { AngularMaterialModule } from 'src/app/angular-material.module';
+import { AvatarModule } from 'primeng/avatar';
+import { InputMaskModule } from "primeng/inputmask";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from 'primeng/button';
+import { ImageModule } from 'primeng/image';
+import { GalleriaModule } from 'primeng/galleria';
+import { CarouselModule } from 'primeng/carousel';
+import { SkeletonModule } from 'primeng/skeleton';
 
 @NgModule({
     imports: [
         CommonModule,
-        SearchRoutingModule
+        SearchRoutingModule,
+        AngularMaterialModule,
+        AvatarModule,
+		InputMaskModule,
+		InputTextareaModule,
+		InputTextModule,
+		ButtonModule,
+        SkeletonModule,
+        ImageModule,
+        GalleriaModule,
+        CarouselModule
     ],
     declarations: [SearchComponent]
 })
