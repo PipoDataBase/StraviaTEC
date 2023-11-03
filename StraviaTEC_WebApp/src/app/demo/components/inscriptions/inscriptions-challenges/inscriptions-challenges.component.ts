fe9ac0f9e525568b509dacfc795e2f11930593e7
Jose Andrés Rodríguez Rojas <joseandres216@gmail.com>
2023-11-03 03:25:39 -0600

    Basic challenge inscription view skeleton
    Done the challenge inscription view skeleton

--- StraviaTEC_WebApp/src/app/demo/components/inscriptions/inscriptions-challenges/inscriptions-challenges.component.ts
+++ StraviaTEC_WebApp/src/app/demo/components/inscriptions/inscriptions-challenges/inscriptions-challenges.component.ts
@@ -1,10 +1,137 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api';
+import { DataView } from 'primeng/dataview';
+import { ProductService } from 'src/app/demo/service/product.service';
+
+export interface Challenge {
+  name: string,
+  goal: number,
+  private: boolean,
+  startDate: string,
+  endDate: string,
+  deep: boolean,
+  type: number
+}
+
+export interface ActivityType {
+  id: number;
+  type: string;
+}
 
 @Component({
   selector: 'app-inscriptions-challenges',
   templateUrl: './inscriptions-challenges.component.html',
   styleUrls: ['./inscriptions-challenges.component.scss']
 })
-export class InscriptionsChallengesComponent {
+export class InscriptionsChallengesComponent implements OnInit {
+  challenges: Challenge[] = [];
+
+  activities: ActivityType[] = []
+
+  sortOptions: SelectItem[] = [];
+
+  sortOrder: number = 0;
+
+  sortField: string = '';
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    // Requests database for all challenges
+    this.challenges = [
+      {
+        name: 'Challenge1',
+        goal: 1000,
+        private: false,
+        startDate: '11/01/2023',
+        endDate: '11/30/2023',
+        deep: true,
+        type: 1
+      },
+      {
+        name: 'Challenge2',
+        goal: 5000,
+        private: false,
+        startDate: '11/01/2023',
+        endDate: '11/30/2023',
+        deep: true,
+        type: 1
+      },
+      {
+        name: 'Challenge3',
+        goal: 10000,
+        private: false,
+        startDate: '11/01/2023',
+        endDate: '11/30/2023',
+        deep: true,
+        type: 1
+      },
+      {
+        name: 'Challenge4',
+        goal: 1000,
+        private: false,
+        startDate: '11/01/2023',
+        endDate: '11/30/2023',
+        deep: false,
+        type: 2
+      },
+      {
+        name: 'Challenge5',
+        goal: 2000,
+        private: false,
+        startDate: '11/01/2023',
+        endDate: '11/30/2023',
+        deep: false,
+        type: 2
+      },
+    ];
+
+    this.activities = [
+      {
+        id: 1,
+        type: 'Runnig'
+      },
+      {
+        id: 2,
+        type: 'Cycling'
+      }
+    ];
+
+    this.sortOptions = [
+      { label: 'Name descending', value: 'name' },
+      { label: 'Name ascending', value: '!name' },
+      { label: 'Goal', value: 'goal' },
+      { label: 'Start date', value: 'startDate' },
+      { label: 'End date', value: 'endDate' }
+    ];
+  }
+
+  onSortChange(event: any) {
+    const value = event.value;
+
+    if (value.indexOf('!') === 0) {
+      this.sortOrder = -1;
+      this.sortField = value.substring(1, value.length);
+    } else {
+      this.sortOrder = 1;
+      this.sortField = value;
+    }
+  }
+
+  onFilter(dv: DataView, event: Event) {
+    dv.filter((event.target as HTMLInputElement).value);
+  }
+
+  joinChallengeButtonOnClick(challengeName: string) {
+    console.log("Joining Challenge " + challengeName)
+  }
+
+  getActivityType(id: number): string {
+    const typeFounded = this.activities.find((type) => type.id == id);
+    if (typeFounded) {
+      return typeFounded.type;
+    }
+    return "";
+  }
 
 }
--- StraviaTEC_WebApp/src/app/demo/components/inscriptions/inscriptions-challenges/inscriptions-challenges.module.ts
+++ StraviaTEC_WebApp/src/app/demo/components/inscriptions/inscriptions-challenges/inscriptions-challenges.module.ts
@@ -2,11 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InscriptionsChallengesRoutingModule } from './inscriptions-challenges-routing.module';
 import { InscriptionsChallengesComponent } from './inscriptions-challenges.component';
+import { DataViewModule } from 'primeng/dataview';
+import { PickListModule } from 'primeng/picklist';
+import { OrderListModule } from 'primeng/orderlist';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { RatingModule } from 'primeng/rating';
+import { ButtonModule } from 'primeng/button';
+import { ChipModule } from 'primeng/chip';
+
 
 @NgModule({
     imports: [
         CommonModule,
-        InscriptionsChallengesRoutingModule
+        InscriptionsChallengesRoutingModule,
+        DataViewModule,
+        PickListModule,
+        OrderListModule,
+        InputTextModule,
+        DropdownModule,
+        RatingModule,
+        ButtonModule,
+        ChipModule
     ],
     declarations: [InscriptionsChallengesComponent]
 })
