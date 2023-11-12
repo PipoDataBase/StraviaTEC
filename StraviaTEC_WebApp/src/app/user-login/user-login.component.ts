6e42806af84132b7868405d7cb056c70a385a44d
MarinGE23 <126499377+MarinGE23@users.noreply.github.com>
2023-11-11 18:59:27 -0600

    Connect login with backend

--- StraviaTEC_WebApp/src/app/user-login/user-login.component.ts
+++ StraviaTEC_WebApp/src/app/user-login/user-login.component.ts
@@ -1,24 +1,47 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { SportmenService } from '../services/sportmen.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.scss']
+  styleUrls: ['./user-login.component.scss'],
+  providers: [MessageService]
 })
 export class UserLoginComponent {
   username: string = '';
   password: string = '';
   hide: boolean = true;
 
-  constructor(private router: Router) { }
+  constructor(private messageService: MessageService, private router: Router, private sportmenService: SportmenService) { }
 
   login(username: string, password: string): void {
-    //validate username and password
-    console.log(username, password);
-    const data = { id: 1, username: username }
-    localStorage.setItem('session', JSON.stringify(data));
-    this.router.navigate(["dashboard"]);
+    if (username == '') {
+      this.messageService.add({ key: 'tc', severity: 'error', summary: 'Error', detail: 'Username is empty.' });
+      return;
+    }
+
+    if (password == '') {
+      this.messageService.add({ key: 'tc', severity: 'error', summary: 'Error', detail: 'Password is empty.' });
+    }
+
+    this.sportmenService.getSportman(username).subscribe({
+      next: (sportman) => {
+        if (sportman.password === password) {
+          const data = { id: 1, username: username }
+          localStorage.setItem('session', JSON.stringify(data));
+          this.router.navigate(["dashboard"]);
+        }
+        else {
+          this.messageService.add({ key: 'tc', severity: 'error', summary: 'Error', detail: 'Incorrect password.' });
+        }
+      },
+      error: (response) => {
+        console.log(response);
+        this.messageService.add({ key: 'tc', severity: 'error', summary: 'Error', detail: 'Username or password incorrect.' });
+      }
+    })
   }
 
   toRegister(): void {
